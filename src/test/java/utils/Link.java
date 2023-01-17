3e82335f76d17bd2ac956714ebdf130d2285bf82
Macarena Acosta <macarenaa988@gmail.com>
2023-01-17 18:11:10 -0300

    add new link to cart page

---
+++ src/test/java/utils/Link.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Link {
+    public static String  HOME ="https://www.amazon.com/";
+    public static String CART = "https://www.amazon.com/gp/cart/view.html?ref_=nav_cart";
+}
