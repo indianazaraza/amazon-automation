8fadea142b92bf61b20dd4fdfcdea063ad56d51d
Macarena Acosta <macarenaa988@gmail.com>
2023-01-17 18:06:59 -0300

    changed testng annotations to cucumber annotations and class name BaseTest to BaseStepDefs

--- src/test/java/steps/BaseStepDefs.java
+++ src/test/java/steps/BaseStepDefs.java
@@ -1,22 +1,25 @@
+package steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import pages.HomePage;
+import pages.CartPage;
+import utils.Link;
 
-public class BaseTest {
+public class BaseStepDefs {
     private WebDriver driver;
-    protected HomePage homePage;
+    protected CartPage cartPage;
 
-    @BeforeTest
+    @Before
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "/home/maca/chromedriver");
         driver = new ChromeDriver();
-        driver.get(Link.home);
-        homePage = new HomePage(driver);
+        driver.get(Link.CART);
+        cartPage = new CartPage(driver);
     }
 
-    @AfterTest
+    @After
     public void tearDown(){
         driver.quit();
     }
