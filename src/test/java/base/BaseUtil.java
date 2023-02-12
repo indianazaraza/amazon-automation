ce1bbda2655bbc1e63b8dd31e4c0f342be1da6c5
Macarena Acosta <macarenaa988@gmail.com>
2023-02-12 20:44:33 -0300

    add the extentReport attribute

--- src/test/java/base/BaseUtil.java
+++ src/test/java/base/BaseUtil.java
@@ -1,7 +1,9 @@
 package base;
 
+import com.aventstack.extentreports.ExtentReports;
 import org.openqa.selenium.WebDriver;
 
 public class BaseUtil {
     public static WebDriver driver;
+    public static ExtentReports extent;
 }
