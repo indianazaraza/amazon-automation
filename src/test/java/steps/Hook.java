69b59641ace94d81dfa76ff01479a0eb7c8a2309
Macarena Acosta <macarenaa988@gmail.com>
2023-02-12 20:50:08 -0300

    deleted BaseUtil attribute and added extentReports instance

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -1,23 +1,34 @@
 package steps;
 
 import base.BaseUtil;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utils.Link;
+import utils.Path;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Hook extends BaseUtil {
-    private BaseUtil base;
 
     @Before
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "/home/maca/chromedriver");
-        base.driver = new ChromeDriver();
-        base.driver.get(Link.CART);
+        System.setProperty("webdriver.chrome.driver", Path.chromeDriver);
+        driver = new ChromeDriver();
+        driver.get(Link.CART);
+        extent = new ExtentReports();
+        var spark = new ExtentSparkReporter("./"+Path.report);
+        extent.attachReporter(spark);
     }
 
     @After
-    public void tearDown(){
-        base.driver.quit();
+    public void tearDown() throws IOException {
+        driver.quit();
+        extent.flush();
+        Desktop.getDesktop().browse(new File(Path.report).toURI());
     }
 }
